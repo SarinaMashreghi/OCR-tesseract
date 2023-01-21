e0eb292986c886c22d0a30d27274b791192f4b93
SarinaMashreghi <sarina.m1384@gmail.com>
2023-01-20 19:49:45 -0500

    putting boxes around words and letters

--- image_to_text.py
+++ image_to_text.py
@@ -1,5 +1,6 @@
 import pytesseract
 import PIL.Image
+from pytesseract import Output
 import cv2
 
 """
@@ -35,3 +36,28 @@ myconfig = r"--psm 6 --oem 3"
 
 text = pytesseract.image_to_string(PIL.Image.open("Images/test_img_3.png"), config=myconfig)
 print(text)
+
+img = cv2.imread("Images/test_img_3.png")
+height, width, _ = img.shape
+
+boxes = pytesseract.image_to_boxes(img, config=myconfig)
+
+# for box in boxes.splitlines():
+#     box = box.split(" ")
+#     img = cv2.rectangle(img, (int(box[1]), height - int(box[2])), (int(box[3]), height - int(box[4])), (255, 0, 0))
+
+# cv2.imshow("img", img)
+# cv2.waitKey(0)
+
+data = pytesseract.image_to_data(img, config=myconfig, output_type=Output.DICT)
+print("data/n", data["text"])
+
+num_boxes = len(data["text"])
+for i in range(num_boxes):
+    if float(data['conf'][i])>80:
+        (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+        img = cv2.rectangle(img, (x, y), (x+width, y+height), (255,0,0))
+        img = cv2.putText(img, data['text'][i], (x, y+height+10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,255,0), 1, 8)
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
